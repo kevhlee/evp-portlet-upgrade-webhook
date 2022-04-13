7053b12595d28a767ca7e85ed6aafd536e6ee9b4
Kevin Lee <kevin.lee@liferay.com>
2022-04-13 11:33:59 -0700

    Prepare project for upgrade via Lugbot CLI

--- liferay-workspace/modules/evp/evp-portlet/src/main/webapp/view.jsp
+++
@@ -1,5 +0,0 @@
-<%@ include file="/init.jsp" %>
-
-<p class="caption">
-	<liferay-ui:message key="evp-portlet.caption" />
-</p>
--- liferay-workspace/modules/evp/evp-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,9 +0,0 @@
-create index IX_FD82FC7F on EVPDivision (companyId, type_);
-
-create unique index IX_7F4A2B1B on EVPEmployee (employeeUserId);
-
-create index IX_BBF5CB96 on EVPGrantRequest (companyId, status);
-
-create index IX_F4BED85F on EVPRequestOrganization (companyId);
-
-create index IX_D78D656F on EVPServiceRequest (companyId, status);
